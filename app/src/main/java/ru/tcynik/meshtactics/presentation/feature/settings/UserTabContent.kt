5bd10c48fcec19e2d8d728b4f8ed76e94fee3a3b
tcynik <tcyniksir@gmail.com>
2026-04-26 15:01:45 +0700

    заменены тогглы на чекбоксы

--- app/src/main/java/ru/tcynik/meshtactics/presentation/feature/settings/UserTabContent.kt
+++ app/src/main/java/ru/tcynik/meshtactics/presentation/feature/settings/UserTabContent.kt
@@ -30,7 +30,7 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Snackbar
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.Switch
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberModalBottomSheetState
@@ -203,7 +203,7 @@ private fun ContourCard(
                 .padding(start = 4.dp, end = 4.dp, top = 8.dp, bottom = 8.dp),
             verticalAlignment = Alignment.CenterVertically,
         ) {
-            Switch(
+            Checkbox(
                 checked = item.isActive,
                 onCheckedChange = onToggleActive,
                 modifier = Modifier.padding(start = 4.dp, end = 8.dp),
