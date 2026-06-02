253317dc1f7d824dbd1f8d3b7b79bb2af7afddd1
tcynik <tcyniksir@gmail.com>
2026-06-02 22:00:38 +0700

    клавиатура теперь нек перекрывает поля имени трека и маркера при правке

--- app/src/main/java/ru/tcynik/meshtactics/presentation/feature/main/osd/TrackRecordingSheet.kt
+++ app/src/main/java/ru/tcynik/meshtactics/presentation/feature/main/osd/TrackRecordingSheet.kt
@@ -19,6 +19,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -118,6 +119,7 @@ fun TrackRecordingSheet(
             Column(
                 modifier = Modifier
                     .fillMaxWidth()
+                    .imePadding()
                     .navigationBarsPadding()
                     .padding(bottom = 8.dp),
             ) {
