e8bc2cccab528b46ba2a25884b1ab969545119ce
tcynik <tcyniksir@gmail.com>
2026-06-15 21:37:53 +0700

    поднял версию кода до 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         applicationId = "ru.tcynik.klitch"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
